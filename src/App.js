49314c288608842e28d160ecb449350e85b4dbaa
Joel Hernandez <joel92_hernandez@outlook.com>
2022-12-01 18:41:08 -0700

    Minor changes, header, hero button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import "./App.css";
-//import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -30,57 +28,23 @@ function App() {
             <ul class="navbar-nav ms-auto navbar-nav-scroll">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#header">
-                  Home
+                  Inicio
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#services">
-                  Services
+                  Sevicios
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#details">
-                  Details
+                  Detalles
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#features">
-                  Features
+                  Caracteristicas
                 </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="dropdown01"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Drop
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="dropdown01">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Article Details
-                    </a>
-                  </li>
-                  <li>
-                    <div class="dropdown-divider"></div>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Terms Conditions
-                    </a>
-                  </li>
-                  <li>
-                    <div class="dropdown-divider"></div>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Privacy Policy
-                    </a>
-                  </li>
-                </ul>
               </li>
             </ul>
             <span class="nav-item">
@@ -102,7 +66,7 @@ function App() {
                   financiera.
                 </p>
                 <a class="btn-solid-lg" href="#services">
-                  Offered services
+                  Nuestros servicios
                 </a>
               </div>
             </div>
